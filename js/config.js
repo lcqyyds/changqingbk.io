ea5ec09e7b7326f4800bc7c9dd21e4870b182dc2
lichangqing-李常青 <97169498+lcqyyds@users.noreply.github.com>
2023-07-05 20:33:57 +0800

    Add files via upload
    Signed-off-by: lichangqing-李常青 <97169498+lcqyyds@users.noreply.github.com>

---
+++ js/config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: N0ts
+ * @Date: 2021-12-03 09:57:35
+ * @LastEditTime: 2021-12-03 10:12:30
+ * @Description: 配置
+ * @FilePath: /NutssssIndex2/js/config.js
+ * @Mail：mail@n0ts.top
+ */
+
+export default {
+    loveTime: Date.UTC(2023, 6, 5), // 恋爱时间
+    giteeName: "licahnqging" // Gitee 账号名
+};
